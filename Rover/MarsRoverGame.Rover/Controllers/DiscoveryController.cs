7f1fad28cb01451379bccf8e66040ecc70a73722
Fatih Göl <fatihgol@yandex.com>
2021-12-29 14:35:56 +0300

    Rover

---
+++ Rover/MarsRoverGame.Rover/Controllers/DiscoveryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarsRoverGame.Rover.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DiscoveryController : ControllerBase
+    {
+        [HttpGet]
+        [ActionName("Ping")]
+        public async Task<IActionResult> Ping()
+        {
+
+            return new ObjectResult("Rover connection ok.")
+            {
+                StatusCode = 200
+            };
+        }
+
+        [HttpPost]
+        [ActionName("Move")]
+        public async Task<IActionResult> Move(string command)
+        {
+
+            return new ObjectResult("Rover Moved")
+            {
+                StatusCode = 200
+            };
+        }
+
+
+        [HttpPost]
+        [ActionName("LeaveRover")]
+        public async Task<IActionResult> LeaveRover()
+        {
+            Environment.Exit(0);
+            return new ObjectResult("")
+            {
+                StatusCode = 200
+            };
+        }
+    }
+}
